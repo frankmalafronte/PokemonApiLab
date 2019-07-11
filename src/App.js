929b4c131868c53edb00d3ddcaf10158c3703861
Frank <frankamalafronte@gmail.com>
2019-07-11 00:26:41 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super()
+    this.state= {pokemon:{}}
+  }
+
+  // async componentDidMount(){
+  //   try{
+  //   const response = await fetch(`https://pokeapi.com/api/v2/pokemon/13`)
+  //   const json = await response.json()
+  //   console.log(json)
+  //   this.setState({pokemon:json})
+  //   } catch(e){
+  //     console.log(e)
+  //   }
+  // }
+
+  async componentDidMount(){
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=150`)
+    const json = await response.json()
+    console.log(json.name)
+    this.setState({pokemon:json})
+  }
+
+  // async componentDidMount() {
+  //   const response = await fetch(
+  //     `https://developer.nps.gov/api/v1/parks?fields=images&api_key=${
+  //       process.env.API_KEY
+  //     }`
+  //   )
+  //   const json = await response.json();
+  //   this.setState({ data: json.data });
+  // }
+  
+  render(){
+    console.log(this.state)
+    // return(<div>{"hello world"}</div>)
+    return(<div>
+        {this.state.pokemon.name}
+    </div>)
+  }
 }
 
 export default App;

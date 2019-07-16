9cda32a2944d6d8add3461258ece702ff12456e5
Frank <frankamalafronte@gmail.com>
2019-07-16 15:28:53 -0400

    testing deployment

--- src/App.js
+++ src/App.js
@@ -7,37 +7,19 @@ class App extends Component {
     this.state= {pokemon:{}}
   }
 
-  // async componentDidMount(){
-  //   try{
-  //   const response = await fetch(`https://pokeapi.com/api/v2/pokemon/13`)
-  //   const json = await response.json()
-  //   console.log(json)
-  //   this.setState({pokemon:json})
-  //   } catch(e){
-  //     console.log(e)
-  //   }
-  // }
+ 
 
   async componentDidMount(){
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=150`)
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/1`)
     const json = await response.json()
     console.log(json.name)
     this.setState({pokemon:json})
   }
 
-  // async componentDidMount() {
-  //   const response = await fetch(
-  //     `https://developer.nps.gov/api/v1/parks?fields=images&api_key=${
-  //       process.env.API_KEY
-  //     }`
-  //   )
-  //   const json = await response.json();
-  //   this.setState({ data: json.data });
-  // }
+
   
   render(){
     console.log(this.state)
-    // return(<div>{"hello world"}</div>)
     return(<div>
         {this.state.pokemon.name}
     </div>)
